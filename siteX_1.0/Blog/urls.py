0139691a7218fdbd4e3217272678b712b7e618f4
spil3141 <johnj2015.jj22@gmail.com>
2018-11-06 05:51:02 +0900

    Back-end rework

--- siteX_1.0/Blog/urls.py
+++ siteX_1.0/Blog/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from . import views
+from .views import index, About
 urlpatterns = [
-    #path('login/login.cgi',views.index, name = "Main Page"),
-    path('',views.index, name = "Main Page"),
-    path('About.html/',views.About, name = "About Page"),
-    path('Contacts.html/',views.Contacts, name='Contacts Page'),
-    path('Signup.html/', views.Signup, name= 'Signup Page'),
-    path('Login.html/', views.Login, name = "Login Page"),
+    path('About/',About, name = "About Page"),
+    path('',index, name = "Main Page"),
 ]
--- siteX_1.0/Blog/views.py
+++ siteX_1.0/Blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 #from .models import account_information, blog_content, favorite_information
 
@@ -7,16 +7,7 @@ def index(request):
     """users = account_information.objects.order_by("user_id")
     output = [s.user_id for s in users]"""
     return render(request,'html/index.html')
-
 def About(request):
-    return render(request,'html/About.html')
+    return render(request, 'html/About.html')
 
-def Contacts(request):
-    return render(request, 'html/Contacts.html')
-
-def Login(request):
-    return render(request, 'html/Login.html')
-
-def Signup(request):
-    return render(request, 'html/Signup.html')
 
