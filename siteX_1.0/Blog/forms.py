6f00f56ee84d47ef994f17e27e092ff871010186
spil3141 <johnj2015.jj22@gmail.com>
2018-11-13 05:47:59 +0900

    debug

--- siteX_1.0/Blog/forms.py
+++ siteX_1.0/Blog/forms.py
@@ -13,7 +13,3 @@ class Signup_form(forms.ModelForm):
 class Login_form(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(max_length=20)
-
-class Search_form(forms.Form):
-    search = forms.CharField(label='', max_length=100)
- 
--- siteX_1.0/Blog/views.py
+++ siteX_1.0/Blog/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from .forms import Signup_form, Login_form,Search_form
+from .forms import Signup_form, Login_form
 
-
-search = Search_form()
 def index(request):
     context = {
-        'search_var': search,
+
     }
     return render(request,'html/index.html',context)
 
@@ -18,23 +16,21 @@ def Signup(request):
         signup_form = Signup_form()
     context = {
         'signup_var': signup_form,
-        'search_var': search,
     }
     return render(request,'html/Signup.html', context)
 
 def Login(request):
     login_form = Login_form(request.POST or None)
+    print (dir(login_form))
     if login_form.is_valid():
         login_form = Login_form()
     context = {
         'login_var': login_form,
-        'search_var': search,
         }
     return render(request,'html/Login.html', context)
 
 def About(request):
     context = {
-        'search_var': search,
     }
     return render(request, 'html/About.html',context)
 
