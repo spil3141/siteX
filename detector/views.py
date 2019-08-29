abf5e163f3d3161e00e5a4e917e267563590a7a8
Seung-Pil Coleman <spil3141@naver.com>
2019-08-29 09:46:23 +0900

    Fixing bug

--- detector/views.py
+++ detector/views.py
@@ -6,9 +6,9 @@ import platform
 from . import models
 from . import forms
 import numpy as np
-from sklearn.externals import joblib
+import joblib
 from . import DataPreprocessing
-import tensorflow as tf
+# import tensorflow as tf
 from django.views.generic import (CreateView,
                                   DetailView,
                                   ListView,
@@ -21,27 +21,33 @@ from django.views.generic import (CreateView,
 
 class Success(DetailView):
     model = models.Item
+    # loaded_model = None
     # Assigning Absolute path based on OS
-    if (platform.system() == "Darwin"):
-        loaded_model_path = joblib.load("/Users/spil3141/Desktop/siteX/detector/static/detector/externals/Trained_Model.sav")
-    elif (platform.system() == "Linux"):
-        loaded_model_path = "/home/spil3141/Desktop/siteX/detector/static/detector/externals/MNIST_Model_10EPOCHS.h5"
-        weights_path = "/home/spil3141/Desktop/siteX/detector/static/detector/externals/cnn_checkpoint.h5"
-        sc_path = "/home/spil3141/Desktop/siteX/detector/static/detector/externals/Scaler_Model.sav"
+    # if (platform.system() == "Darwin"):
+    #     loaded_model_path = joblib.load("/Users/spil3141/Desktop/siteX/detector/static/detector/externals/Trained_Model.sav")
+    # elif (platform.system() == "Linux"):
+    #     loaded_model_path = "/home/spil3141/Desktop/siteX/detector/static/detector/externals/MNIST_Model_10EPOCHS.h5"
+    #     weights_path = "/home/spil3141/Desktop/siteX/detector/static/detector/externals/cnn_checkpoint.h5"
+    #     sc_path = "/home/spil3141/Desktop/siteX/detector/static/detector/externals/Scaler_Model.sav"
+        # #Restoring Model
+        # loaded_model = tf.keras.models.load_model(loaded_model_path)
+        # loaded_model.load_weights(weights_path)
+        # loaded_stdsc = DataPreprocessing.Scale_with_loaded_sc(sc_path)
+    # else:
+        # loaded_model_path = "C:/Users/spil3141/Desktop/siteX/detector/static/detector/externals/MNIST_Model_10EPOCHS.h5"
+        # weights_path = "C:/Users/spil3141/Desktop/siteX/detector/static/detector/externals/cnn_checkpoint.h5"
+        # sc_path = "C:/Users/spil3141/Desktop/siteX/detector/static/detector/externals/Scaler_Model.sav"
+        # pass
 
-    #Restoring Model
-    loaded_model = tf.keras.models.load_model(loaded_model_path)
-    loaded_model.load_weights(weights_path)
-    loaded_stdsc = DataPreprocessing.Scale_with_loaded_sc(sc_path)
 
     def get_object(self):
         obj = super().get_object()
         #Converting img to numpy 1d array
         # img = spil.img_2_1d_arr(obj.image) -> Returns the path to the image of focus
-        img_std = DataPreprocessing.flatten_2d_to_4d(DataPreprocessing.Scale_with_loaded_sc(DataPreprocessing.img_2_flatten_2d(obj.image),self.sc_path))
 
         #using the classifier to make prediction
-        if self.clf:
+        if self.loaded_model != None:
+            img_std = DataPreprocessing.flatten_2d_to_4d(DataPreprocessing.Scale_with_loaded_sc(DataPreprocessing.img_2_flatten_2d(obj.image),self.sc_path))
             self.prediction = str(np.argmax(self.loaded_model.predict(img_std),axis = 1))
         else:
             self.prediction = "failed to load"
@@ -79,5 +85,3 @@ class Upload(CreateView):
     # else:
     #     form = forms.ItemForm()
     # return render(request,"detector/Upload.html",{"form":form,"success":success})
-
-
